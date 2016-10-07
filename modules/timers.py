88aeae7d530522baec6014e4d8bbf9df68a7a204
meadowdrone@gmail.com <meadowdrone@gmail.com>
2016-10-07 10:57:20 -0400

    significantly cleaned up timers code

--- modules/timers.py
+++ modules/timers.py
@@ -26,17 +26,26 @@ def timers():
     daily_hours, daily_remainder = divmod(daily_delta.seconds, 3600)
     daily_minutes, daily_seconds = divmod(daily_remainder, 60)
     
-    dhstr = " hour, " if daily_hours == 1 else " hours, "
-    dmstr = " minute " if daily_minutes == 1 else " minutes, "
-    dsstr = " second" if daily_seconds == 1 else " seconds"
+    dhstr = str(daily_hours) + " hour, " if daily_hours == 1 else str(daily_hours) + " hours, "
+    dmstr = str(daily_minutes) + " minute " if daily_minutes == 1 else str(daily_minutes) + " minutes, "
+    dsstr = str(daily_seconds) + " second" if daily_seconds == 1 else str(daily_seconds) + " seconds"
 
-    # weekly timer calculation
-    if (day_of_week_now == 0):
+    # weekly + scrip timer calculation
+    if day_of_week_now == 0:
         days_until_weekly = 1
-    elif (day_of_week_now == 1):
+        days_until_scrip = 3
+    elif day_of_week_now == 1:
         days_until_weekly = 0 if current_date.hour < 4 else 7
+        days_until_scrip = 2
+    elif day_of_week_now == 2:
+        days_until_weekly = 8 - day_of_week_now
+        days_until_scrip = 1
+    elif day_of_week_now == 3:
+        days_until_weekly = 8 - day_of_week_now
+        days_until_scrip = 0 if current_date.hour < 4 else 7
     else:
         days_until_weekly = 8 - day_of_week_now
+        days_until_scrip = 10 - day_of_week_now
 
     weekly_timer = datetime.datetime(
         current_date.year,
@@ -53,64 +62,32 @@ def timers():
     weekly_minutes, weekly_seconds = divmod(weekly_remainder, 60)
     weekly_days = weekly_delta.days + days_until_weekly
         
-    wdstr = " day, " if weekly_days == 1 else " days, "
-    whstr = " hour, " if weekly_hours == 1 else " hours, "
-    wmstr = " minute" if weekly_minutes == 1 else " minutes"
-    wsstr = " second" if weekly_seconds == 1 else " seconds"
-
-    # scrip reset calculation
-    if (day_of_week_now == 0):
-        days_until_scrip = 3
-    elif (day_of_week_now == 1):
-        days_until_scrip = 2
-    elif (day_of_week_now == 2):
-        days_until_scrip = 1
-    elif (day_of_week_now == 3):
-        days_until_scrip = 0 if current_date.hour < 4 else 7
-    else:
-        days_until_scrip = 10 - day_of_week_now
-
-    scrip_timer = datetime.datetime(
-        current_date.year,
-        current_date.month,
-        current_date.day,
-        4,
-        0,
-        0)
-
-    scrip_delta = scrip_timer - current_date
+    wdstr = str(weekly_days) + " day, " if weekly_days == 1 else str(weekly_days) + " days, "
+    whstr = str(weekly_hours) + " hour, " if weekly_hours == 1 else str(weekly_hours) + " hours, "
+    wmstr = str(weekly_minutes) + " minute" if weekly_minutes == 1 else str(weekly_minutes) + " minutes"
+    wsstr = str(weekly_seconds) + " second" if weekly_seconds == 1 else str(weekly_seconds) + " seconds"
 
     # get scrip seconds, minutes & hours
-    scrip_hours, scrip_remainder = divmod(scrip_delta.seconds, 3600)
+    scrip_hours, scrip_remainder = divmod(weekly_delta.seconds, 3600)
     scrip_minutes, scrip_seconds = divmod(scrip_remainder, 60)
-    scrip_days = scrip_delta.days + days_until_scrip
+    scrip_days = weekly_delta.days + days_until_scrip
     
-    sdstr = " day, " if scrip_days == 1 else " days, "
-    shstr = " hour, " if scrip_hours == 1 else " hours, "
-    smstr = " minute" if scrip_minutes == 1 else " minutes"
-    ssstr = " second" if scrip_seconds == 1 else " seconds"
+    sdstr = str(scrip_days) + " day, " if scrip_days == 1 else str(scrip_days) + " days, "
+    shstr = str(scrip_hours) + " hour, " if scrip_hours == 1 else str(scrip_hours) + " hours, "
+    smstr = str(scrip_minutes) + " minute" if scrip_minutes == 1 else str(scrip_minutes) + " minutes"
+    ssstr = str(scrip_seconds) + " second" if scrip_seconds == 1 else str(scrip_seconds) + " seconds"
 
     # return results
-    if (weekly_days == 0):
-        timer = "%s%s%s%s and %s%s until daily reset\n" % (
-            str(daily_hours), dhstr, str(daily_minutes), dmstr, str(daily_seconds), dsstr)
-        timer += "%s%s%s%s and %s%s until weekly reset\n" % (
-            str(weekly_hours), whstr, str(weekly_minutes), wmstr, str(weekly_seconds), wsstr)
-        timer += "%s%s%s%s%s%s and %s%s until scrip and grand company reset" % (str(
-            scrip_days), sdstr, str(scrip_hours), shstr, str(scrip_minutes), smstr, str(scrip_seconds), ssstr)
-    elif (scrip_delta.days == 0):
-        timer = "%s%s%s%s and %s%s until daily reset\n" % (
-            str(daily_hours), dhstr, str(daily_minutes), dmstr, str(daily_seconds), dsstr)
-        timer += "%s%s%s%s%s%s and %s%s until weekly reset\n" % (str(weekly_days), wdstr, str(
-            weekly_hours), whstr, str(weekly_minutes), wmstr, str(weekly_seconds), wsstr)
-        timer += "%s%s%s%s%s and %s%s until scrip and grand company reset" % (str(scrip_days), sdstr, str(
-            scrip_hours), shstr, str(scrip_minutes), smstr, str(scrip_seconds), ssstr)
+    timer = "%s%s and %s until daily reset\n" % (dhstr, dmstr, dsstr)
+    
+    if weekly_days == 0:
+        timer += "%s%s and %s until weekly reset\n" % (whstr, wmstr, wsstr)
+        timer += "%s%s%s and %s until scrip and grand company reset" % (sdstr, shstr, smstr, ssstr)
+    elif scrip_days == 0:
+        timer += "%s%s%s and %s until weekly reset\n" % (wdstr, whstr, wmstr, wsstr)
+        timer += "%s%s%s and %s until scrip and grand company reset" % (sdstr, shstr, smstr, ssstr)
     else:
-        timer = "%s%s%s%s and %s%s until daily reset\n" % (
-            str(daily_hours), dhstr, str(daily_minutes), dmstr, str(daily_seconds), dsstr)
-        timer += "%s%s%s%s%s%s and %s%s until weekly reset\n" % (str(weekly_days), wdstr, str(
-            weekly_hours), whstr, str(weekly_minutes), wmstr, str(weekly_seconds), wsstr)
-        timer += "%s%s%s%s%s%s and %s%s until scrip and grand company reset" % (str(
-            scrip_days), sdstr, str(scrip_hours), shstr, str(scrip_minutes), smstr, str(scrip_seconds), ssstr)
+        timer += "%s%s%s and %s until weekly reset\n" % (wdstr, whstr, wmstr, wsstr)
+        timer += "%s%s%s and %s until scrip and grand company reset" % (sdstr, shstr, smstr, ssstr)
 
     return timer
